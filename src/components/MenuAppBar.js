207a745f59686a7bf85266998ee5c9ef2b400e89
Jonathan Volker <81990006+jonathanvolker@users.noreply.github.com>
2022-11-12 17:27:43 -0300

    dos terminarios

--- src/components/MenuAppBar.js
+++ src/components/MenuAppBar.js
@@ -74,8 +74,6 @@ export default function MenuAppBar({ handleBtnClick }) {
     setAnchorEl(null);
   };
 
- 
-
   const handleSignOut = async () => {
     // console.log("logging out");
     await logout(dispatch);
@@ -83,12 +81,7 @@ export default function MenuAppBar({ handleBtnClick }) {
     // setAnchorEl(null);
   };
 
-  const redirectOnSignOut = () => {
-    history.push("/login");
-  };
-
-
- 
+  const user = JSON.parse(localStorage.getItem("currentUser")).user;
 
   const closeModal = () => {
     setModal(false);
@@ -96,253 +89,261 @@ export default function MenuAppBar({ handleBtnClick }) {
   React.useEffect(() => {
     if (state.user === "") {
       console.log("redirecting on sign out");
-     // redirectOnSignOut();
+      // redirectOnSignOut();
     }
   }, []);
-  
+
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{width:'100%', border: 1, borderColor:"black", bgcolor: "#000000", padding: "0.5% 0" }}>
-        <Toolbar>
-          <div onClick={handleBtnClick}>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{
-                "& svg": {
-                  fontSize: "35px",
-                  color: "#ebe9eb",
-                  fill: "#ebe9eb",
-                },
-              }}
-              onClick={() => setIsDrawerOpen(true)}
-              >
-              <MenuIcon />
-              
-            </IconButton>
-            <Drawer 
-            anchor = 'left'
-            open={isDrawerOpen}
-            onClose={()=> setIsDrawerOpen(false)}>
-
-            <Box p={2} width='100%' height='100%' textAlign='center' role='presentation'
-            sx={{ bgcolor: "#0a0b0c"}}
-            
-            >
-                <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{
-                "& svg": {
-                  fontSize: "35px",
-                  color: "#ebe9eb",
-                  fill: "#ebe9eb",
-                },
-                mr: 2,
-              }}
-              onClick={() => setIsDrawerOpen(false)}
-              >
-              <KeyboardBackspaceIcon />
-              
-            </IconButton>
-            <Typography
-                  variant="h6"
-                  component="div"
-                  className="main-title"
-                  sx={{
-                    flexGrow: 1,
-                    fontSize: "30px",
-                    marginBottom: "4%",
-                    borderBottom: 1,
-                    borderColor: "white",
-                    color:"white",
-                    "@media screen and (max-width: 768px)": {
-                      fontSize: "14px",
-                    },
-                  }}
-                  onClick={handleHomeClick}
-                  >
-                Home
-                </Typography>
-                <Typography
-                  variant="h6"
-                  component="div"
-                  className="main-title"
-                  sx={{
-                    flexGrow: 1,
-                    fontSize: "30px",
-                    marginBottom: "4%",
-                    borderBottom: 1,
-                    borderColor: "white",
-                    color:"white",
-                    "@media screen and (max-width: 768px)": {
-                      fontSize: "14px",
-                    },
-                  }}
-                  onClick={handlePlayerClick}
-                  >
-                Players
-                </Typography>   
-
-                <Typography
-                variant="h6"
-                component="div"
-                className="main-title"
-                sx={{
-                  flexGrow: 1,
-                  fontSize: "30px",
-                  marginBottom: "4%",
-                  borderBottom: 1,
-                  borderColor: "white",
-                  color:"white",
-                  "@media screen and (max-width: 768px)": {
-                    fontSize: "14px",
-                  },
-                }}
-                onClick={handleGroupClick}
-                >
-              Groups
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                className="main-title"
-                sx={{
-                  flexGrow: 1,
-                  fontSize: "30px",
-                  marginBottom: "4%",
-                  borderBottom: 1,
-                  borderColor: "white",
-                  color:"white",
-                  "@media screen and (max-width: 768px)": {
-                    fontSize: "14px",
-                  },
-                }}
-                onClick={handlePlayerStatsClick}
-                >
-              Player Statistics
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                className="main-title"
-                sx={{
-                  flexGrow: 1,
-                  fontSize: "30px",
-                  marginBottom: "4%",
-                  borderBottom: 1,
-                  borderColor: "white",
-                  color:"white",
-                  "@media screen and (max-width: 768px)": {
-                    fontSize: "14px",
-                  },
-                }}
-                onClick={handleGroupStatsClick}
-                >
-              Group Statistics
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                className="main-title"
-                sx={{
-                  flexGrow: 1,
-                  fontSize: "30px",
-                  marginBottom: "4%",
-                  borderBottom: 1,
-                  borderColor: "white",
-                  color:"white",
-                  "@media screen and (max-width: 768px)": {
-                    fontSize: "14px",
-                  },
-                }}
-                onClick={handleTournamentsClick}
-                >
-              Tournaments
-              </Typography>
-
-            </Box>
-            </Drawer>
-          </div>
-
-          <Typography
-                variant="h6"
-                component="div"
-                className="main-title"
-                sx={{
-                  fontSize: "30px",
-                  marginLeft: "2%",
-                  color:"white",
-                  "@media screen and (max-width: 768px)": {
-                    fontSize: "14px",
-                  },
-                }}
-                onClick={handleUSClick}
-                >
-              Users
-              </Typography>
-
-          
-          {auth && (
-            <div className="avatar-container">
-              <Typography >{seller}</Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ width: '100%', border: 1, borderColor: "black", bgcolor: "#000000", padding: "0.5% 0" }}>
+          <Toolbar>
+            <div onClick={handleBtnClick}>
               <IconButton
                 size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
+                edge="start"
                 color="inherit"
+                aria-label="menu"
                 sx={{
-                  position: "absolute",
-                  right: "1%",
                   "& svg": {
                     fontSize: "35px",
-                    color: "black",
-                    fill: "black",
+                    color: "#ebe9eb",
+                    fill: "#ebe9eb",
                   },
-                  mr:2,
                 }}
+                onClick={() => setIsDrawerOpen(true)}
+              >
+                <MenuIcon />
+
+              </IconButton>
+              <Drawer
+                anchor='left'
+                open={isDrawerOpen}
+                onClose={() => setIsDrawerOpen(false)}>
+
+                <Box p={2} width='100%' height='100%' textAlign='center' role='presentation'
+                  sx={{ bgcolor: "#0a0b0c" }}
+
                 >
-                <Avatar src={avatar} alt="PF" />
-              </IconButton >
-              <Menu
-                id="acoount-menu"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                >
-                {/* <MenuItem onClick={handleClose}>Profile</MenuItem> */}
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
-              </Menu>
+                  <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{
+                      "& svg": {
+                        fontSize: "35px",
+                        color: "#ebe9eb",
+                        fill: "#ebe9eb",
+                      },
+                      mr: 2,
+                    }}
+                    onClick={() => setIsDrawerOpen(false)}
+                  >
+                    <KeyboardBackspaceIcon />
+
+                  </IconButton>
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    className="main-title"
+                    sx={{
+                      flexGrow: 1,
+                      fontSize: "30px",
+                      marginBottom: "4%",
+                      borderBottom: 1,
+                      borderColor: "white",
+                      color: "white",
+                      "@media screen and (max-width: 768px)": {
+                        fontSize: "14px",
+                      },
+                    }}
+                    onClick={handleHomeClick}
+                  >
+                    Home
+                  </Typography>
+
+
+
+
+                  {user.admin ? (<>
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="main-title"
+                      sx={{
+                        flexGrow: 1,
+                        fontSize: "30px",
+                        marginBottom: "4%",
+                        borderBottom: 1,
+                        borderColor: "white",
+                        color: "white",
+                        "@media screen and (max-width: 768px)": {
+                          fontSize: "14px",
+                        },
+                      }}
+                      onClick={handlePlayerClick}
+                    >
+                      Players
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="main-title"
+                      sx={{
+                        flexGrow: 1,
+                        fontSize: "30px",
+                        marginBottom: "4%",
+                        borderBottom: 1,
+                        borderColor: "white",
+                        color: "white",
+                        "@media screen and (max-width: 768px)": {
+                          fontSize: "14px",
+                        },
+                      }}
+                      onClick={handleGroupClick}
+                    >
+                      Groups
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="main-title"
+                      sx={{
+                        flexGrow: 1,
+                        fontSize: "30px",
+                        marginBottom: "4%",
+                        borderBottom: 1,
+                        borderColor: "white",
+                        color: "white",
+                        "@media screen and (max-width: 768px)": {
+                          fontSize: "14px",
+                        },
+                      }}
+                      onClick={handlePlayerStatsClick}
+                    >
+                      Player Statistics
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="main-title"
+                      sx={{
+                        flexGrow: 1,
+                        fontSize: "30px",
+                        marginBottom: "4%",
+                        borderBottom: 1,
+                        borderColor: "white",
+                        color: "white",
+                        "@media screen and (max-width: 768px)": {
+                          fontSize: "14px",
+                        },
+                      }}
+                      onClick={handleGroupStatsClick}
+                    >
+                      Group Statistics
+                    </Typography>
+
+                  </>)
+                    : null}
+
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    className="main-title"
+                    sx={{
+                      flexGrow: 1,
+                      fontSize: "30px",
+                      marginBottom: "4%",
+                      borderBottom: 1,
+                      borderColor: "white",
+                      color: "white",
+                      "@media screen and (max-width: 768px)": {
+                        fontSize: "14px",
+                      },
+                    }}
+                    onClick={handleTournamentsClick}
+                  >
+                    Tournaments
+                  </Typography>
+
+                </Box>
+              </Drawer>
             </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-    {modal ? (
+
+            <Typography
+              variant="h6"
+              component="div"
+              className="main-title"
+              sx={{
+                fontSize: "30px",
+                marginLeft: "2%",
+                color: "white",
+                "@media screen and (max-width: 768px)": {
+                  fontSize: "14px",
+                },
+              }}
+              onClick={handleUSClick}
+            >
+              Users
+            </Typography>
+
+
+            {auth && (
+              <div className="avatar-container">
+                <Typography >{seller}</Typography>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                  sx={{
+                    position: "absolute",
+                    right: "1%",
+                    "& svg": {
+                      fontSize: "35px",
+                      color: "black",
+                      fill: "black",
+                    },
+                    mr: 2,
+                  }}
+                >
+                  <Avatar src={avatar} alt="PF" />
+                </IconButton >
+                <Menu
+                  id="acoount-menu"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  {/* <MenuItem onClick={handleClose}>Profile</MenuItem> */}
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                  <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {modal ? (
         <AdminDashboard
           closeModal={closeModal}
         />
       ) : null}
-          </>
+    </>
   );
 }
