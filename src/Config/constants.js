ead690ee5bd24400f3c9335ed2bed50a59ae63d6
Jose Ignacio Camats <54079915+Josicamats@users.noreply.github.com>
2022-11-24 17:39:26 -0300

    Update constants.js

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,14 +1,5 @@
 
-//export const ROOT_URL = "https://bbzlatam-backend.onrender.com" // aws
-export const ROOT_URL = 'http://localhost:8080' // localhost;
-//export const ROOT_URL = "http://168.181.185.148:8080" // vm donweb
-export const STATUS = [
-  "EN ESPERA",
-  "EN PROCESO",
-  "ENTREGADO",
-  "CANCELADO",
-  "CONSIGNA",
-];
-export const COMISSIONSTATUS = ["POR PAGAR", "PAGADO", "CANCELADO", ""];
-export const COMISSIONAUTHORIZATION = ["APROBADO", "NO APROBADO", ""];
-export const WORK = ["Maquinado", "Soldadura", "Suministro"];
+//export const ROOT_URL = "https://bbzlatam-backend.onrender.com" // back (env: dev) render.com
+//export const ROOT_URL = 'http://localhost:8080' // localhost;
+export const ROOT_URL = "http://168.181.185.148:8080" // vm donweb
+
