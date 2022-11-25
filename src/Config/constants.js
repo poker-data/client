abf016ec350cc6f577dc0e18717257fddde127b1
josicamats <jose_Camats_22@hotmail.com>
2022-11-25 02:08:41 -0300

    root a VM

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,7 +1,7 @@
 
 
-export const ROOT_URL = "https://bbzlatam-backend.onrender.com" // back (env: dev) render.com
+//export const ROOT_URL = "https://bbzlatam-backend.onrender.com" // back (env: dev) render.com
 //export const ROOT_URL = 'http://localhost:8080' // localhost;
-//export const ROOT_URL = "http://168.181.185.148:8080" // vm donweb
+export const ROOT_URL = "http://168.181.185.148:8080" // vm donweb
 
 
