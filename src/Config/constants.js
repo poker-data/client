2dab4002cbc0dc9034543a42cf935804a23e1f66
Jonathan Volker <81990006+jonathanvolker@users.noreply.github.com>
2023-07-30 18:36:18 -0300

    Update constants.js

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,4 +1,4 @@
 //export const ROOT_URL = 'http://localhost:8080' // localhost;
 //export const ROOT_URL = "http://168.181.185.148:8080" // vm donweb
-export const ROOT_URL = 'http://54.91.126.153:8081' //vm aws
+export const ROOT_URL = 'https://54.91.126.153:8081' //vm aws
 //export const ROOT_URL = "https://bbzlatam-backend.onrender.com"// back (env: dev) render.com
